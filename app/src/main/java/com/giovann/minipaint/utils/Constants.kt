0a4596cf246d7c64c9991bc59e0c429b2da5172f
flyingfrisbee <giovannhiu@gmail.com>
2022-04-22 21:52:57 +0700

    TODO: cache move in db, scale drawing with canvas size, add in app update

--- app/src/main/java/com/giovann/minipaint/utils/Constants.kt
+++ app/src/main/java/com/giovann/minipaint/utils/Constants.kt
@@ -1,7 +1,6 @@
 package com.giovann.minipaint.utils
 
 object Constants {
-    const val BASE_URL = "https://ws-scribbler.herokuapp.com"
-    const val WEBSOCKET_URL = "wss://ws-scribbler.herokuapp.com/ws/"
+
     const val STROKE_WIDTH = 12f
 }
